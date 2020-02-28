0a58b6e62f65cfb2d605177f33720b2eb8c38d24
Sam <Jeffcoat>
2020-02-27 18:35:35 -0600

    es8 exercise done

---
+++ es8/script.js
@@ -0,0 +1,33 @@
+.padStart()// string methods to add padding
+.padEnd()
+
+
+
+const fun = (a, b, c, d, ) {
+  console.log(a)
+}
+
+fun(1, 2, 3, 4, )
+
+
+Object.values
+Object.entries
+
+
+let obj = {
+  username0 : 'Santa',
+  username1: 'Rudolph',
+  username2: 'Mr. Grinch'
+}
+
+Object.keys(obj).forEach(value=> {
+  console.log(value);
+})
+
+Object.entries(obj).forEach(value=> {
+  console.log(value);
+})
+
+Object.entries(obj).map(value => {
+  return value[1] + value[0].replace('username', '')
+});
